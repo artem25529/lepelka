81b7eb21602f8b574908938f85fe03ead5b08c8a
artsi <artsi@mail.com>
2023-05-17 23:30:04 +0300

    master

---
+++ src/main/java/com/es/lepelka/LepelkaApplication.java
@@ -0,0 +1,13 @@
+package com.es.lepelka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LepelkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LepelkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/es/lepelka/LepelkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.lepelka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LepelkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
